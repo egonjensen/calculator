440c670f25f976eda7b95d102be825981ac5d9e2
Egon Jensen <ej@interform400.com>
2018-01-02 12:35:31 +0100

    Acceptance Test

--- acceptence_test.sh
+++ acceptence_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
-test $(curl 192.168.99.100:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+./gradlew acceptanceTest -Dcalculator.url=http://localhost:$CALCULATOR_PORT
 
